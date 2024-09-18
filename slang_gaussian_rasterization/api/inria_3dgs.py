e197661bdf095e9dc95d795ee3b98d992908ed39
Chinmay Talegaonkar <ctalegaonkar@ucsd.edu>
2024-09-18 03:44:30 +0000

    messed up commit

--- slang_gaussian_rasterization/api/inria_3dgs.py
+++ slang_gaussian_rasterization/api/inria_3dgs.py
@@ -17,10 +17,10 @@ Inria 3DGS code-base https://github.com/graphdeco-inria/gaussian-splatting"""
 import torch
 from slang_gaussian_rasterization.internal.alphablend_tiled_slang import render_alpha_blend_tiles_slang_raw
 
-def common_properties_from_inria_GaussianModel(gaussian_model):
+def common_properties_from_inria_GaussianModel(gaussian_model, opacity_volr = False):
   """ Fetches all the Gaussian properties from the inria defined Gaussian Model object"""
   xyz_ws = gaussian_model.get_xyz
-  opacity = gaussian_model.get_opacity
+  opacity = gaussian_model.get_opacity_volr if opacity_volr else gaussian_model.get_opacity
   rotations = gaussian_model.get_rotation
   scales = gaussian_model.get_scaling
   sh_coeffs = gaussian_model.get_features
@@ -59,4 +59,24 @@ def render(viewpoint_camera, pc, pipe, bg_color, scaling_modifier = 1.0, overrid
                                                   fovy, fovx, height, width)
   
   return render_pkg
- 
+
+def render_volr(viewpoint_camera, pc, pipe, bg_color, scaling_modifier = 1.0, override_color = None):
+  """ Implements the volumetric rasterization Interface defined in the inria code-base."""
+  assert scaling_modifier == 1.0, "scaling_modifier is not supported in the slang-gaussian-rasterization."
+  assert override_color is None, "override_color is not support in the slang-gaussian-rasterization."
+  assert pipe.convert_SHs_python is False, "convert_SHs_python is not supported."
+  assert pipe.compute_cov3D_python is False, "compute_cov3D_python is not supported."
+  assert pipe.debug is False, "debug mode is not supported."
+  assert torch.equal(bg_color, torch.zeros_like(bg_color)), "only black background is supported currently."
+
+  active_sh = pc.active_sh_degree
+  xyz_ws, rotations, scales, sh_coeffs, opacity = common_properties_from_inria_GaussianModel(pc, opacity_volr=True)
+  world_view_transform, proj_mat, cam_pos, fovy, fovx, height, width = common_properties_from_inria_Camera(viewpoint_camera)  
+
+
+  render_pkg = render_alpha_blend_tiles_slang_raw(xyz_ws, rotations, scales, opacity, 
+                                                  sh_coeffs, active_sh,
+                                                  world_view_transform, proj_mat, cam_pos,
+                                                  fovy, fovx, height, width)
+  
+  return render_pkg
