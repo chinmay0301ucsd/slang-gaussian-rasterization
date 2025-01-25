ade9aa393347980b89e4b5b58c1b573a725575c2
Chinmay Talegaonkar <chinmay0301@gmail.com>
2025-01-25 21:12:03 +0000

    supporting tile thresh parameters

--- slang_gaussian_rasterization/internal/alphablend_tiled_slang.py
+++ slang_gaussian_rasterization/internal/alphablend_tiled_slang.py
@@ -25,7 +25,7 @@ def set_grad(var):
 def render_alpha_blend_tiles_slang_raw(xyz_ws, rotations, scales, opacity, 
                                        sh_coeffs, active_sh,
                                        world_view_transform, proj_mat, cam_pos,
-                                       fovy, fovx, height, width, softplus_rgb, tile_size=16):
+                                       fovy, fovx, height, width, softplus_rgb, tile_size=16, use_new_tile_size=False, tilethresh=0.005):
     
     scale3d_factor = torch.ones_like(opacity, device=opacity.device)
     render_grid = RenderGrid(height,
@@ -36,6 +36,8 @@ def render_alpha_blend_tiles_slang_raw(xyz_ws, rotations, scales, opacity,
                                                                                            rotations,
                                                                                            scales,
                                                                                            scale3d_factor,
+                                                                                           opacity,
+                                                                                           tilethresh,
                                                                                            sh_coeffs,
                                                                                            active_sh,
                                                                                            world_view_transform,
@@ -44,7 +46,8 @@ def render_alpha_blend_tiles_slang_raw(xyz_ws, rotations, scales, opacity,
                                                                                            fovy,
                                                                                            fovx,
                                                                                            render_grid,
-                                                                                           softplus_rgb)
+                                                                                           softplus_rgb,
+                                                                                           use_new_tile_size)
     # print("num gaussians on all tiles", len(sorted_gauss_idx))
     # print("num gaussians per tile", len(sorted_gauss_idx) / radii.shape[0])
     # retain_grad fails if called with torch.no_grad() under evaluation
